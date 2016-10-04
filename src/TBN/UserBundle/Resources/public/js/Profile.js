5c30859995f6c4f24fb28f9a6b97db52372c9e94
Guillaume SAINTHILLIER <guillaume.sainthillier@gmail.com>
2016-10-04 20:01:35 +0200

    Correction profile navigation

--- src/TBN/UserBundle/Resources/public/js/Profile.js
+++ src/TBN/UserBundle/Resources/public/js/Profile.js
@@ -1,12 +1,19 @@
-var Profile = {
+var Profile;
+Profile = {
+    settings: {
+        offsetTop: 75,
+        scrollSpeed: 400,
+        easingType: 'linear'
+    },
     init: function () {
         $(function () {
-            $("#shorcuts li a").click(function() {
-                console.log('OK');
-                return true;
+            $("#shorcuts li a").click(function () {
+                var elem = $($(this).attr('href'));
+                $.scrollTo(elem.offset().top - Profile.settings.offsetTop, Profile.settings.scrollSpeed, {easing: Profile.settings.easingType});
+                return false;
             })
-            $("#btnDelete").click(function() {
-               $("#modalDelete").modal();
+            $("#btnDelete").click(function () {
+                $("#modalDelete").modal();
             });
         });
     }
