6a23328c612da390a96146b60144603017a71bf3
Guillaume SAINTHILLIER <guillaume.sainthillier@gmail.com>
2016-11-10 20:23:08 +0100

    Suppressions namespace non utilisés

--- src/TBN/UserBundle/Form/Type/ProfileFormType.php
+++ src/TBN/UserBundle/Form/Type/ProfileFormType.php
@@ -12,14 +12,12 @@
 namespace TBN\UserBundle\Form\Type;
 
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use FOS\UserBundle\Form\Type\ProfileFormType as BaseType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
-use TBN\MainBundle\Form\Type\ImageType;
 use Vich\UploaderBundle\Form\Type\VichImageType;
 
 class ProfileFormType extends BaseType
