f74de2a48c8a7f9b8fa6fb77a9a3a3da42cafb13
Guillaume SAINTHILLIER <guillaume.sainthillier@gmail.com>
2016-12-20 18:34:00 +0100

    Correction fichier migration

--- app/DoctrineMigrations/Version20161217172214.php
+++ app/DoctrineMigrations/Version20161217172214.php
@@ -20,13 +20,10 @@ class Version20161217172214 extends AbstractMigration
 
         $this->addSql('ALTER TABLE Agenda ADD system_path VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE User ADD system_path VARCHAR(255) DEFAULT NULL');
-        $this->addSql('UPDATE Agenda SET system_path = path, path = NULL WHERE user_id IS NULL');
-        /**
-         *   UPDATE `User` u
-         *   LEFT JOIN `Info` i ON u.info_id = u.id
-         *   SET u.system_path = u.path, u.path = NULL
-         *   WHERE LENGTH(i.facebook_id) = 0
-         */
+        $this->addSql('UPDATE Agenda SET system_path = path WHERE user_id IS NULL');
+        $this->addSql('UPDATE Agenda SET path = NULL WHERE user_id IS NULL');
+        $this->addSql('UPDATE User SET system_path = path WHERE salt IS NULL');
+        $this->addSql('UPDATE User SET path = NULL WHERE salt IS NULL');
     }
 
     /**
@@ -37,6 +34,7 @@ class Version20161217172214 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
+        $this->addSql('UPDATE User SET path = system_path WHERE salt IS NULL');
         $this->addSql('UPDATE Agenda SET path = system_path WHERE user_id IS NULL');
         $this->addSql('ALTER TABLE Agenda DROP system_path');
         $this->addSql('ALTER TABLE User DROP system_path');
