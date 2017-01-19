792c20370242cdf0942bac51b3d7a6bbcbd25528
Guillaume SAINTHILLIER <guillaume.sainthillier@gmail.com>
2017-01-20 00:13:40 +0100

    Fix unloadable app

--- src/TBN/MainBundle/Twig/MainExtension.php
+++ src/TBN/MainBundle/Twig/MainExtension.php
@@ -62,6 +62,15 @@ class MainExtension extends \Twig_Extension implements \Twig_Extension_GlobalsIn
 
     public function getGlobals()
     {
+        $site = $this->siteManager->getCurrentSite();
+
+        if(! $site) {
+            return [
+                "site" => null,
+                "siteInfo" => null
+            ];
+        }
+        
         return [
             "site" => $this->siteManager->getCurrentSite(),
             "siteInfo" => $this->siteManager->getSiteInfo()
