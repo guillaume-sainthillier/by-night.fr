8245c5ff8d36af6657ef73d046dd3de4f7ee58b2
Guillaume SAINTHILLIER <guillaume.sainthillier@gmail.com>
2017-02-19 20:31:21 +0100

    Revert "Fix place event"
    This reverts commit ee2d33d111dad4831de21b5d318dfa9c7279582a.

--- src/TBN/AgendaBundle/Controller/MenuDroitController.php
+++ src/TBN/AgendaBundle/Controller/MenuDroitController.php
@@ -87,13 +87,6 @@ class MenuDroitController extends Controller
         }
         $soiree = $result;
 
-        if(! $soiree->getPlace()) {
-            return $this->redirectToRoute('tbn_agenda_details', [
-                'id' => $soiree->getId(),
-                'slug' => $soiree->getSlug(),
-            ]);
-        }
-
         $em = $this->getDoctrine()->getManager();
         $repo = $em->getRepository("TBNAgendaBundle:Agenda");
 
