b558a6d9bd12ac30f6946a58d10b42ad601d01ab
Guillaume SAINTHILLIER <guillaume.sainthillier@gmail.com>
2017-05-08 20:14:21 +0200

    Add item to sitemap

--- src/AppBundle/EventListener/SitemapSuscriber.php
+++ src/AppBundle/EventListener/SitemapSuscriber.php
@@ -34,6 +34,11 @@ class SitemapSuscriber implements EventSubscriberInterface
     private $urlContainer = null;
 
     /**
+     * @var \DateTime
+     */
+    private $now;
+
+    /**
      * @param UrlGeneratorInterface $urlGenerator
      * @param ObjectManager         $manager
      */
@@ -41,6 +46,7 @@ class SitemapSuscriber implements EventSubscriberInterface
     {
         $this->urlGenerator = $urlGenerator;
         $this->manager = $manager;
+        $this->now = new \DateTime();
     }
 
     /**
@@ -56,15 +62,42 @@ class SitemapSuscriber implements EventSubscriberInterface
     public function registerRoutes(SitemapPopulateEvent $event)
     {
         $this->urlContainer = $event->getUrlContainer();
-        $this->registerStaticRoutes();
-        $this->registerEventRoutes();
+        $section = $event->getSection();
+
+        $sections = [
+            'app' => [$this, 'registerStaticRoutes'],
+            'agenda' => [$this, 'registerAgendaRoutes'],
+            'users' => [$this, 'registerUserRoutes'],
+//            'events' => [$this, 'registerEventRoutes']
+        ];
+
+        foreach($sections as $name => $generateFunction) {
+            if(!$section || $name === $section) {
+                call_user_func($generateFunction, $name);
+            }
+        }
     }
 
-    private function registerEventRoutes() {
-        $events = $this->manager->getRepository("AppBundle:Agenda")->findSiteMap();
+    private function registerAgendaRoutes($section) {
+        $sites = $this->manager->getRepository("AppBundle:Site")->findAll();
 
+        foreach($sites as $site) {
+            $this->addUrl($section, "tbn_agenda_agenda", ["city" => $site->getSubdomain()]);
+            $this->addUrl($section, "tbn_agenda_sortir", ["type" => "concert", "city" => $site->getSubdomain()]);
+            $this->addUrl($section, "tbn_agenda_sortir", ["type" => "etudiant", "city" => $site->getSubdomain()]);
+            $this->addUrl($section, "tbn_agenda_sortir", ["type" => "famille", "city" => $site->getSubdomain()]);
+            $this->addUrl($section, "tbn_agenda_sortir", ["type" => "spectacle", "city" => $site->getSubdomain()]);
+            $this->addUrl($section, "tbn_agenda_sortir", ["type" => "exposition", "city" => $site->getSubdomain()]);
+        }
+
+        $places = $this->manager->getRepository("AppBundle:Place")->findAll();
+        foreach($places as $place) {
+            $this->addUrl($section, "tbn_agenda_place", ["slug" => $place->getSlug(), "city" => $place->getSite()->getSubdomain()]);
+        }
+
+        $events = [];
         foreach($events as $event) {
-            $this->addUrl("events", "tbn_agenda_details", [
+            $this->addUrl($section, "tbn_agenda_details", [
                 "id" => $event['id'],
                 "slug" => $event['slug'],
                 "city" => $event['subdomain'],
@@ -72,25 +105,52 @@ class SitemapSuscriber implements EventSubscriberInterface
         }
     }
 
-    private function registerStaticRoutes() {
+    private function registerEventRoutes($section) {
+        $events = $this->manager->getRepository("AppBundle:Agenda")->findSiteMap();
+
+        foreach($events as $event) {
+            $this->addUrl($section, "tbn_agenda_details", [
+                "id" => $event['id'],
+                "slug" => $event['slug'],
+                "city" => $event['subdomain'],
+            ]);
+        }
+
+        //TODO: add tags to sitemap
+    }
+
+    private function registerUserRoutes($section) {
+        $users = $this->manager->getRepository("AppBundle:User")->findAll();
+
+        foreach($users as $user) {
+            $this->addUrl($section, "tbn_user_details", [
+                "id" => $user->getId(),
+                "slug" => $user->getSlug()
+            ], $user->getUpdatedAt());
+        }
+    }
+
+    private function registerStaticRoutes($section) {
         $staticRoutes = [
             'tbn_main_index',
             'tbn_agenda_about',
             'tbn_agenda_plus',
-            'tbn_main_cookie'
+            'tbn_main_cookie',
+            'fos_user_security_login',
+            'fos_user_registration_register',
         ];
 
         foreach($staticRoutes as $route) {
-            $this->addUrl("app", $route);
+            $this->addUrl($section, $route);
         }
     }
 
-    private function addUrl($section, $name, array $params = []) {
+    private function addUrl($section, $name, array $params = [], \DateTime $lastMod = null) {
         $url = $this->urlGenerator->generate($name, $params, UrlGeneratorInterface::ABSOLUTE_URL);
 
         $url = new UrlConcrete(
             $url,
-            new \DateTime(),
+            $lastMod ?: $this->now,
             UrlConcrete::CHANGEFREQ_HOURLY,
             1
         );
