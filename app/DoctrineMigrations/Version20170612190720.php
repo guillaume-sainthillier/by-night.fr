2b41b4b9145bb7879b9bf23fab6877405ed6472f
Guillaume SAINTHILLIER <guillaume.sainthillier@gmail.com>
2017-06-12 21:53:15 +0200

    Fix incorrect index on events

---
+++ app/DoctrineMigrations/Version20170612190720.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Application\Migrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20170612190720 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX agenda_fb_participations ON agenda');
+        $this->addSql('CREATE INDEX agenda_fb_participations ON agenda (date_fin, fb_participations, fb_interets)');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('DROP INDEX agenda_fb_participations ON Agenda');
+        $this->addSql('CREATE INDEX agenda_fb_participations ON Agenda (fb_participations, fb_interets)');
+    }
+}
