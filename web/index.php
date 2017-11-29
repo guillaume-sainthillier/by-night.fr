45da051d8fdb333747f132100baf98ccabc784de
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2017-11-29 12:24:29 +0000

    Apply fixes from StyleCI

--- web/index.php
+++ web/index.php
@@ -5,19 +5,18 @@ use Symfony\Component\Debug\Debug;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-if (getenv('APP_DEBUG')) {
-
+if (\getenv('APP_DEBUG')) {
     // Deny if client address is remote and is not in a container
-    if (!in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'], true) && false === getenv('SYMFONY_ALLOW_APPDEV')) {
-        header('HTTP/1.0 403 Forbidden');
-        exit('You are not allowed to access this file. Check ' . basename(__FILE__) . ' for more information.');
+    if (!\in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1'], true) && false === \getenv('SYMFONY_ALLOW_APPDEV')) {
+        \header('HTTP/1.0 403 Forbidden');
+        exit('You are not allowed to access this file. Check ' . \basename(__FILE__) . ' for more information.');
     }
 
     Debug::enable();
 }
 
-$kernel = new AppKernel(getenv('APP_ENV'), getenv('APP_DEBUG'));
-$request = Request::createFromGlobals();
+$kernel   = new AppKernel(\getenv('APP_ENV'), \getenv('APP_DEBUG'));
+$request  = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
