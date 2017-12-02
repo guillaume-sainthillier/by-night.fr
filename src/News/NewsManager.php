15981928c92af42aeb77dd63657bdfd5bde3b938
Guillaume SAINTHILLIER <guillaume.sainthillier@gmail.com>
2017-12-02 16:22:26 +0100

    Fix PHPdoc

--- src/News/NewsManager.php
+++ src/News/NewsManager.php
@@ -85,7 +85,7 @@ class NewsManager
         foreach ($datas as $site => $events) {
             $participants[$site] = 0;
             foreach ($events as $event) {
-                /*
+                /**
                  * @var Agenda $event
                  */
                 $participants[$site] += $event->getFbInterets() + $event->getFbParticipations();
