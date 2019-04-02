88a91e84dbdfe7ce6219b52771069aa7e50da452
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2019-04-02 21:57:50 +0200

    RIP Google+ part 2

---
+++ src/Migrations/Version20190402195719.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20190402195719 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Agenda DROP google_post_id, DROP google_post_system_id');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Agenda ADD google_post_id VARCHAR(127) DEFAULT NULL COLLATE utf8_unicode_ci, ADD google_post_system_id VARCHAR(127) DEFAULT NULL COLLATE utf8_unicode_ci');
+    }
+}
