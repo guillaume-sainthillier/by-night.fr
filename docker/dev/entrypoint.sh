7bf2e417ee0d0fd020e025b2739c09e118cbea26
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2019-10-12 17:47:53 +0200

    Removed worker for dev

--- docker/dev/entrypoint.sh
+++ docker/dev/entrypoint.sh
@@ -4,9 +4,5 @@ set -e
 if [ $# -eq 0 ]; then
     supervisord -c /etc/supervisor/conf.d/supervisord.conf
 else
-    if [ "$1" = "worker" ]; then
-        supervisord -c /etc/supervisor/conf.d/supervisord-worker.conf
-    else
-        exec "$@"
-    fi
+    exec "$@"
 fi
