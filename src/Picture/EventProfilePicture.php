fce29f8d30e9a00694343160c94d0005716dc492
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2019-11-12 21:13:59 +0100

    Added default parser images

--- src/Picture/EventProfilePicture.php
+++ src/Picture/EventProfilePicture.php
@@ -3,6 +3,11 @@
 namespace App\Picture;
 
 use App\Entity\Event;
+use App\Parser\Common\EventBriteParser;
+use App\Parser\Common\OpenAgendaParser;
+use App\Parser\Common\SowProgParser;
+use App\Parser\Toulouse\BikiniParser;
+use App\Parser\Toulouse\ToulouseParser;
 use App\Twig\AssetExtension;
 use Symfony\Component\Asset\Packages;
 use Vich\UploaderBundle\Templating\Helper\UploaderHelper;
@@ -43,6 +48,26 @@ class EventProfilePicture
             );
         }
 
+        if ($event->getFromData() === BikiniParser::getParserName()) {
+            return $this->packages->getUrl('build/images/parsers/bikini.jpg');
+        }
+
+        if ($event->getFromData() === ToulouseParser::getParserName()) {
+            return $this->packages->getUrl('build/images/parsers/toulouse-tourisme.jpg');
+        }
+
+        if ($event->getFromData() === EventBriteParser::getParserName()) {
+            return $this->packages->getUrl('build/images/parsers/eventbrite.jpg');
+        }
+
+        if ($event->getFromData() === SowProgParser::getParserName()) {
+            return $this->packages->getUrl('build/images/parsers/sowprog.jpg');
+        }
+
+        if ($event->getFromData() === OpenAgendaParser::getParserName()) {
+            return $this->packages->getUrl('build/images/parsers/openagenda.jpg');
+        }
+
         if ($event->getUrl()) {
             return $event->getUrl();
         }
@@ -60,6 +85,41 @@ class EventProfilePicture
 
         if ($event->getSystemPath()) {
             return $this->assetExtension->thumb($this->helper->asset($event, 'systemFile'), $params);
+        }
+
+        if ($event->getFromData() === BikiniParser::getParserName()) {
+            return $this->assetExtension->thumbAsset(
+                $this->packages->getUrl('build/images/parsers/bikini.jpg', 'local'),
+                $params
+            );
+        }
+
+        if ($event->getFromData() === ToulouseParser::getParserName()) {
+            return $this->assetExtension->thumbAsset(
+                $this->packages->getUrl('build/images/parsers/toulouse-tourisme.jpg', 'local'),
+                $params
+            );
+        }
+
+        if ($event->getFromData() === EventBriteParser::getParserName()) {
+            return $this->assetExtension->thumbAsset(
+                $this->packages->getUrl('build/images/parsers/eventbrite.jpg', 'local'),
+                $params
+            );
+        }
+
+        if ($event->getFromData() === SowProgParser::getParserName()) {
+            return $this->assetExtension->thumbAsset(
+                $this->packages->getUrl('build/images/parsers/sowprog.jpg', 'local'),
+                $params
+            );
+        }
+
+        if ($event->getFromData() === OpenAgendaParser::getParserName()) {
+            return $this->assetExtension->thumbAsset(
+                $this->packages->getUrl('build/images/parsers/openagenda.jpg', 'local'),
+                $params
+            );
         }
 
         return $this->assetExtension->thumbAsset(
