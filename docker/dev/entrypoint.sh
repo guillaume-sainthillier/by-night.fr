70e0dfe77d1f35b5d73cf02ef8cd0d1dabc16e24
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2019-11-28 20:48:45 +0100

    Improved Dockerfiles (prod & dev)

--- docker/dev/entrypoint.sh
+++ docker/dev/entrypoint.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
 set -e
 
-uid=$(stat -c %u /app)
-gid=$(stat -c %g /app)
-
 if [ $# -eq 0 ]; then
     supervisord -c /etc/supervisor/conf.d/supervisord.conf
 else
     exec "$@"
-fi
-
-sed -i "s/user = www-data/user = bynight/g" /usr/local/etc/php-fpm.d/www.conf
-sed -i "s/group = www-data/group = bynight/g" /usr/local/etc/php-fpm.d/www.conf
-sed -i -r "s/bynight:x:[0-9]+:[0-9]+:/bynight:x:$uid:$gid:/g" /etc/passwd
-sed -i -r "s/bynight:x:[0-9]+:/bynight:x:$gid:/g" /etc/group
-
-chown $uid:$gid /home/bynight
-
-if [ $# -eq 0 ]; then
-    supervisord -c /etc/supervisor/conf.d/supervisord.conf
-else
-    exec gosu bynight "$@"
 fi
