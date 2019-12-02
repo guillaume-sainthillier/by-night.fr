68ec0f168feea7b1740a8a8fc0d41ef39e0c5715
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2019-12-02 16:12:26 +0100

    Removed old slug index

---
+++ migrations/Version20191202142932.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20191202142932 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Agenda DROP INDEX UNIQ_2B41CD41989D9B62, ADD INDEX event_slug_idx (slug)');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE Agenda DROP INDEX event_slug_idx, ADD UNIQUE INDEX UNIQ_2B41CD41989D9B62 (slug)');
+    }
+}
