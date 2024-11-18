2359580571b8d776ea887021c45c0a17f8490291
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2024-11-18 15:51:49 +0100

    upgrade eslint from 8 to 9

---
+++ eslint.config.mjs
@@ -0,0 +1,24 @@
+import globals from 'globals'
+import pluginJs from '@eslint/js'
+import eslintConfigPrettier from 'eslint-config-prettier'
+
+export default [
+    {
+        languageOptions: {
+            globals: {
+                ... globals.browser,
+                ... globals.node,
+            },
+        },
+    },
+    pluginJs.configs.recommended,
+    eslintConfigPrettier,
+    {
+        ignores: ["vendor/", "public/"],
+    },
+    {
+        rules: {
+            semi: ['error', 'never'],
+        },
+    },
+]
