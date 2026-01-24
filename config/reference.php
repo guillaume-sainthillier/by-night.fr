8b33cd58f4c3630587d50c09d400cf72133d5187
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2026-01-24 11:43:01 +0100

    disable elasticsearch during tests

--- config/reference.php
+++ config/reference.php
@@ -1,6 +1,12 @@
 <?php
 
-// This file is auto-generated and is for apps only. Bundles SHOULD NOT rely on its content.
+/*
+ * This file is part of By Night.
+ * (c) 2013-present Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
