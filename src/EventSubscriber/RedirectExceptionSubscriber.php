e945639f740c1f59658813c3876205a0aac3e12e
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2026-01-31 10:50:21 +0100

    refactor event & user redirection handling

---
+++ src/EventSubscriber/RedirectExceptionSubscriber.php
@@ -0,0 +1,42 @@
+<?php
+
+/*
+ * This file is part of By Night.
+ * (c) 2013-present Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+namespace App\EventSubscriber;
+
+use App\Exception\RedirectException;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+/**
+ * Converts RedirectException into RedirectResponse.
+ */
+final class RedirectExceptionSubscriber implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+
+    public function onKernelException(ExceptionEvent $event): void
+    {
+        $exception = $event->getThrowable();
+
+        if (!$exception instanceof RedirectException) {
+            return;
+        }
+
+        $response = new RedirectResponse($exception->getUrl(), $exception->getStatusCode());
+        $event->setResponse($response);
+    }
+}
