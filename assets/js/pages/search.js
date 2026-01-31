1d6261cb075953249c1aff46db81654d15ad6f0c
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2026-01-31 19:11:06 +0100

    remove tom-select in search page

--- assets/js/pages/search.js
+++ assets/js/pages/search.js
@@ -1,8 +1,6 @@
 import $ from 'jquery'
 import initImagePreview from '@/js/lazy-listeners/image-previews'
-import initSelects from '@/js/lazy-listeners/selects'
 
 $(document).ready(function () {
     initImagePreview()
-    initSelects()
 })
