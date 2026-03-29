ccd9b936aa204538058e9cc974789188cfaa97b4
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2026-03-29 12:49:23 +0200

    implements pagination for events

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -194,13 +194,20 @@ final class EventRepository extends ServiceEntityRepository implements DtoFindab
             ->toIterable();
     }
 
-    public function findAllByUserQueryBuilder(User $user): QueryBuilder
+    public function findAllByUserQueryBuilder(User $user, ?string $q = null): QueryBuilder
     {
-        return $this
+        $qb = $this
             ->createQueryBuilder('e')
             ->where('e.user = :user')
             ->setParameter('user', $user->getId())
             ->orderBy('e.id', Criteria::DESC);
+
+        if ($q) {
+            $qb->andWhere('e.name LIKE :q')
+                ->setParameter('q', '%' . $q . '%');
+        }
+
+        return $qb;
     }
 
     public function getCountryEvents(): array
