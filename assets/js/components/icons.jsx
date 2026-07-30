f4e12dae3426585ea9ece3f8a6c777426448e8b4
Guillaume Sainthillier <guillaume.sainthillier@gmail.com>
2026-07-30 11:26:55 +0200

    fix missing h function

--- assets/js/components/icons.jsx
+++ assets/js/components/icons.jsx
@@ -1,3 +1,4 @@
+import { h } from 'preact'
 import { render } from 'preact-render-to-string'
 
 export const iconHtml = (Icon, className = '') => render(h(Icon, { className: `icon ${className}`.trim() }))
